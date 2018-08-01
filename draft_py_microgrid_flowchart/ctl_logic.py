f9cd4dd4749ac50b3dadf7399f981eb83b889fe3
Ali <yazdanpanah.aly@gmail.com>
2018-08-01 16:50:35 +0430

    OO AC/DC_Hub, Battery, Load, Grid, PV

--- draft_py_microgrid_flowchart/ctl_logic.py
+++ draft_py_microgrid_flowchart/ctl_logic.py
@@ -1,29 +1,50 @@
 import random
 import datetime
 
-class Microgrid:
-    def __init__(self, pwr_grid, pwr_load, pwr_battery, pwr_pv, soc):
-        self.pwr_grid = pwr_grid
-        self.pwr_load = pwr_load
+
+class DC_Hub:
+    def __init__(self,battery,pv):
+        self.pv = pv
+        self.battery = battery
+
+
+class Battery:
+    def __init__(self,pwr_battery,soc):
         self.pwr_battery = pwr_battery
-        self.pwr_pv = pwr_pv
         self.soc = soc
         self.max_battery_capacity = 10000 #watt
-        self.nodes = []
-        self.next_node = Node(0,self)
-        
-    def set_pwr_load(self, number):
-        self.pwr_load = number
+    
+    def set_soc(self,arg):
+        self.soc = arg
 
-    def get_pwr_load():
-        return self.pwr_load
-
-    def set_pwr_battery(self, number):
-        self.pwr_battery = number
-
+    def get_soc(self):
+        return self.soc 
+    
+    def set_pwr_battery(self,arg):
+        self.pwr_battery = arg
+    
     def get_pwr_battery(self):
         return self.pwr_battery
 
+    def get_max_battery_capacity(self):
+        return self.max_battery_capacity
+
+    def battery_health(self):
+        pass
+    def charge_battery (self,*args):
+        temp = ""
+        for num in args:
+            if type(num) == int:
+                temp += " " +  str(num) + " " 
+        print("charge battery" + temp)
+            
+
+class PV:
+    def __init__(self):
+        self.pwr_pv = 0
+
+    def get_pwr_pv(self):
+        return self.pwr_pv
     def set_pwr_pv(self):
         localhour = datetime.datetime.now().hour
         localminute = datetime.datetime.now().minute
@@ -38,54 +59,20 @@ class Microgrid:
         elif (localhour >= 18) & (localhour <= 23 ):
             self.pwr_pv = 0 #watt (no sun)
 
-    def get_pwr_pv(self):
-        return self.pwr_pv
+class AC_Hub:
+    def __init__(self,grid,load):
+        self.load = load
+        self.grid = grid
 
-    def set_soc(self, number):
-        self.soc = number
+class Grid:
+    def __init__(self,pwr_grid):
+        self.pwr_grid = pwr_grid
     
-    def get_soc(self):
-        return self.soc
-
-    def get_max_battery_capacity(self):
-        return self.max_battery_capacity
-
-    def set_pwr_grid(self, number):
-        self.pwr_grid = number
+    def set_pwr_grid(self,pwr_grid):
+        self.pwr_grid = pwr_grid
     
     def get_pwr_grid(self):
         return self.pwr_grid
-
-    def add_node(self, n):
-        self.nodes.append(n)
-
-    def delete_node(self, n):
-        if n in self.nodes:
-            self.nodes.remove(n)
-
-    def set_next_node(self, n):
-        self.next_node = n
-
-    def charge_battery (*args):
-        temp = ""
-        for num in args:
-            if type(num) == int:
-                temp += " " +  str(num) + " " 
-        print("charge battery" + temp)
-        
-    def supply_load (*args):
-        temp = ""
-        for a in args:
-            if type(a) == int:
-                temp += " " +  str(a) + " "
-        print("supply load" + temp)
-
-    def sell_load (*args):
-        temp = ""
-        for a in args:
-            if type(a) == int:
-                temp +=  " " + str(a) + " "
-        print("sell load" + temp)
 
     def grid_state_mode(self):
         localhour = datetime.datetime.now().hour
@@ -100,6 +87,68 @@ class Microgrid:
             print("selling")
             return "selling"
         
+class Load:
+    def __init__(self,pwr_load):
+        self.pwr_load = pwr_load
+
+    def supply_load (self,*args):
+        temp = ""
+        for a in args:
+            if type(a) == int:
+                temp += " " +  str(a) + " "
+        print("supply load" + temp)
+
+    def set_pwr_load(self,arg):
+        self.pwr_load = arg
+    
+    def get_pwr_load(self):
+        return self.pwr_load
+
+    def prediction(self):
+        pass
+    
+
+
+class Microgrid:
+    def __init__(self,e_sell,e_buy,ac_hub,dc_hub):
+        self.ac_hub = ac_hub
+        self.dc_hub = dc_hub
+        self.nodes = []
+        self.next_node = Node(0,self)
+        self.e_sell = e_sell
+        self.e_buy = e_buy
+
+    def get_e_sell(self):
+        return self.e_sell
+
+    def set_e_sell(self,arg):
+        self.e_sell = arg
+    
+    def get_e_buy(self):
+        return self.e_buy
+
+    def set_e_buy(self,arg):
+        self.e_buy = arg
+
+    def add_node(self, n):
+        self.nodes.append(n)
+
+    def delete_node(self, n):
+        if n in self.nodes:
+            self.nodes.remove(n)
+
+    def set_next_node(self, n):
+        self.next_node = n
+
+    def sell_load (self,*args):
+        temp = ""
+        for a in args:
+            if type(a) == int:
+                self.e_sell += a
+                temp +=  " " + str(a) + " "
+        print("sell load" + temp)
+
+
 
 class Node:
     def __init__(self, id_number, microgrid):
@@ -115,29 +164,29 @@ class Node:
 
 class Node1(Node):
     def function(self):
-        if self.microgrid.pwr_load > 0:
+        if self.microgrid.ac_hub.load.get_pwr_load() > 0:
             self.goto(3)
         else:
             self.goto(2)
 
 class Node2(Node):
     def function(self):
-        if self.microgrid.pwr_pv > 0:
+        if self.microgrid.dc_hub.pv.get_pwr_pv() > 0:
             self.goto(4)
         else:
             self.goto(9)
 
 class Node3(Node):
     def function(self):
-        if self.microgrid.pwr_pv >= self.microgrid.pwr_load:
-            self.microgrid.supply_load(self.microgrid.pwr_pv)
+        if self.microgrid.dc_hub.pv.get_pwr_pv() >= self.microgrid.ac_hub.load.get_pwr_load():
+            self.microgrid.ac_hub.load.supply_load(self.microgrid.dc_hub.pv.get_pwr_pv)
             self.goto(4)
         else:
             self.goto(16)
 
 class Node4(Node):
     def function(self):
-        state = self.microgrid.grid_state_mode()
+        state = self.microgrid.ac_hub.grid.grid_state_mode()
         if state == "buying":
             self.goto(7)
         elif state == "selling":
@@ -147,41 +196,41 @@ class Node4(Node):
 
 class Node5(Node):
     def function(self):
-        if self.microgrid.soc > 70:
-            self.microgrid.sell_load(self.microgrid.pwr_pv, self.microgrid.pwr_battery)
+        if self.microgrid.dc_hub.battery.get_soc() > 70:
+            self.microgrid.sell_load(self.microgrid.dc_hub.pv.get_pwr_pv(), self.microgrid.dc_hub.battery.get_pwr_battery())
         else:
-            self.microgrid.sell_load(self.microgrid.pwr_pv)
+            self.microgrid.sell_load(self.microgrid.dc_hub.pv.get_pwr_pv())
         self.goto(1)
 
 class   Node6(Node):
     def function(self):
-        if self.microgrid.soc > 90:
-            self.microgrid.sell_load(self.microgrid.pwr_battery)
+        if self.microgrid.dc_hub.battery.get_soc() > 90:
+            self.microgrid.sell_load(self.microgrid.dc_hub.battery.get_pwr_battery())
             self.goto(1)
         else:
-            self.microgrid.charge_battery(self.microgrid.pwr_pv)
+            self.microgrid.dc_hub.battery.charge_battery(self.microgrid.dc_hub.pv.get_pwr_pv())
             self.goto(8)
 
 class Node7(Node):
     def function(self):
-        if self.microgrid.soc == 100:
-            self.microgrid.sell_load(self.microgrid.pwr_battery)
+        if self.microgrid.dc_hub.battery.get_soc() == 100:
+            self.microgrid.sell_load(self.microgrid.dc_hub.battery.get_pwr_battery())
             self.goto(1)
         else:
-            self.microgrid.charge_battery(self.microgrid.pwr_pv, self.microgrid.pwr_grid)
+            self.microgrid.dc_hub.battery.charge_battery(self.microgrid.dc_hub.pv.get_pwr_pv(), self.microgrid.ac_hub.grid.get_pwr_grid())
             self.goto(8)
 
 class Node8(Node):
     def function(self):
-        if self.microgrid.pwr_pv > 0:
-            self.microgrid.sell_load(self.microgrid.pwr_battery)
-            self.goto(1)
+        if self.microgrid.dc_hub.pv.get_pwr_pv() > 0:
+            self.microgrid.sell_load(self.microgrid.dc_hub.battery.get_pwr_battery())
         else:
-            self.goto(1)
+            pass
+        self.goto(1)
 
 class Node9(Node):
     def function(self):
-        state = self.microgrid.grid_state_mode()
+        state = self.microgrid.ac_hub.grid.grid_state_mode()
         if state == "buying":
             self.goto(10)
         elif state == "selling":
@@ -191,55 +240,55 @@ class Node9(Node):
 
 class Node10(Node):
     def function(self):
-        if self.microgrid.soc == 100:
+        if self.microgrid.dc_hub.battery.get_soc() == 100:
             pass
         else:
-            self.microgrid.charge_battery(self.microgrid.pwr_grid)
+            self.microgrid.dc_hub.battery.charge_battery(self.microgrid.ac_hub.grid.get_pwr_grid())
         self.goto(1)
 
 class Node11(Node):
     def function(self):
-        if self.microgrid.soc > 70:
-            self.microgrid.sell_load(self.microgrid.pwr_battery)
+        if self.microgrid.dc_hub.battery.get_soc() > 70:
+            self.microgrid.sell_load(self.microgrid.dc_hub.battery.get_pwr_battery())
         else:
             pass
         self.goto(1)
 
 class Node12(Node):
     def function(self):
-        if self.microgrid.pwr_battery >+ (self.microgrid.pwr_load - self.microgrid.pwr_pv):
-            self.microgrid.sell_load(self.microgrid.pwr_battery)
+        if self.microgrid.dc_hub.battery.get_pwr_battery() >= (self.microgrid.ac_hub.load.get_pwr_load() - self.microgrid.dc_hub.pv.get_pwr_pv()):
+            self.microgrid.sell_load(self.microgrid.dc_hub.battery.get_pwr_battery())
         else:
-            self.microgrid.supply_load(self.microgrid.pwr_pv, self.microgrid.pwr_battery, self.microgrid.pwr_grid)
+            self.microgrid.ac_hub.load.supply_load(self.microgrid.dc_hub.pv.get_pwr_pv(), self.microgrid.dc_hub.battery.get_pwr_battery(), self.microgrid.ac_hub.grid.get_pwr_grid())
         self.goto(1)
 
 class Node13(Node):
     def function(self):
-        if self.microgrid.pwr_battery >= (self.microgrid.pwr_load - self.microgrid.pwr_pv):
-            self.microgrid.supply_load(self.microgrid.pwr_pv, self.microgrid.pwr_battery)
+        if self.microgrid.dc_hub.battery.get_pwr_battery() >= (self.microgrid.pwr_load - self.microgrid.dc_hub.pv.get_pwr_pv()):
+            self.microgrid.ac_hub.load.supply_load(self.microgrid.dc_hub.pv.get_pwr_pv(), self.microgrid.dc_hub.battery.get_pwr_battery())
         else:
-            self.microgrid.supply_load(self.microgrid.pwr_pv, self.microgrid.pwr_battery, self.microgrid.pwr_grid)
+            self.microgrid.ac_hub.load.supply_load(self.microgrid.dc_hub.pv.get_pwr_pv(), self.microgrid.dc_hub.battery.get_pwr_battery(), self.microgrid.ac_hub.grid.get_pwr_grid())
         self.goto(1)
 
 class Node14(Node):
     def function(self):
-        if self.microgrid.soc > 70:
+        if self.microgrid.dc_hub.battery.get_soc() > 70:
             self.goto(12)
         else:
-            self.microgrid.supply_load(self.microgrid.pwr_pv, self.microgrid.pwr_battery, self.microgrid.pwr_grid)
+            self.microgrid.ac_hub.load.supply_load(self.microgrid.dc_hub.pv.get_pwr_pv(), self.microgrid.dc_hub.battery.get_pwr_battery(), self.microgrid.ac_hub.grid.get_pwr_grid())
             self.goto(1)
 
 class Node15(Node):
     def function(self):
-        if self.microgrid.soc > 0:
+        if self.microgrid.dc_hub.battery.get_soc() > 0:
             self.goto(13)
         else:
-            self.microgrid.supply_load(self.microgrid.pwr_pv, self.microgrid.pwr_grid)
+            self.microgrid.ac_hub.load.supply_load(self.microgrid.dc_hub.pv.get_pwr_pv(), self.microgrid.ac_hub.grid.get_pwr_grid())
             self.goto(1)
 
 class Node16(Node):
     def function(self):
-        state = self.microgrid.grid_state_mode()
+        state = self.microgrid.ac_hub.grid.grid_state_mode()
         if state == "buying":
             self.goto(17)
         elif state == "selling":
@@ -249,9 +298,9 @@ class Node16(Node):
 
 class Node17(Node):
     def function(self):
-        if self.microgrid.soc == 100:
-            self.microgrid.supply_load(self.microgrid.pwr_pv, self.microgrid.pwr_grid)
+        if self.microgrid.dc_hub.battery.get_soc() == 100:
+            self.microgrid.ac_hub.load.supply_load(self.microgrid.dc_hub.pv.get_pwr_pv(), self.microgrid.ac_hub.grid.get_pwr_grid())
         else:
-            self.microgrid.charge_battery(self.microgrid.pwr_grid)
+            self.microgrid.dc_hub.battery.charge_battery(self.microgrid.ac_hub.grid.get_pwr_grid())
         self.goto(1)
 
--- draft_py_microgrid_flowchart/simulator.py
+++ draft_py_microgrid_flowchart/simulator.py
@@ -1,79 +1,86 @@
 from ctl_logic import *
 import random
 
-test_grid = Microgrid(random.randrange(101),random.randrange(101),random.randrange(101),random.randrange(101),random.randrange(101))
+test_pv = PV()
+test_battery = Battery(random.randrange(0,101),random.randrange(0,101))
+test_load = Load(random.randrange(0,101))
+test_grid = Grid(random.randrange(0,101))
+test_ac = AC_Hub(test_grid,test_load)
+test_dc = DC_Hub(test_battery,test_pv)
+
+test_microgrid = Microgrid(0,0,test_ac,test_dc)
 
 
 #creating and adding nodes
 
-n1 = Node1(1,test_grid)
-test_grid.add_node(n1)
+n1 = Node1(1,test_microgrid)
+test_microgrid.add_node(n1)
 
-n2 = Node2(2,test_grid)
-test_grid.add_node(n2)
+n2 = Node2(2,test_microgrid)
+test_microgrid.add_node(n2)
 
-n3 = Node3(3,test_grid)
-test_grid.add_node(n3)
+n3 = Node3(3,test_microgrid)
+test_microgrid.add_node(n3)
 
-n4 = Node4(4,test_grid)
-test_grid.add_node(n4)
+n4 = Node4(4,test_microgrid)
+test_microgrid.add_node(n4)
 
-n5 = Node5(5,test_grid)
-test_grid.add_node(n5)
+n5 = Node5(5,test_microgrid)
+test_microgrid.add_node(n5)
 
-n6 = Node6(6,test_grid)
-test_grid.add_node(n6)
+n6 = Node6(6,test_microgrid)
+test_microgrid.add_node(n6)
 
-n7 = Node7(7,test_grid)
-test_grid.add_node(n7)
+n7 = Node7(7,test_microgrid)
+test_microgrid.add_node(n7)
 
-n8 = Node8(8,test_grid)
-test_grid.add_node(n8)
+n8 = Node8(8,test_microgrid)
+test_microgrid.add_node(n8)
 
-n9 = Node9(9,test_grid)
-test_grid.add_node(n9)
+n9 = Node9(9,test_microgrid)
+test_microgrid.add_node(n9)
 
-n10 = Node10(10,test_grid)
-test_grid.add_node(n10)
+n10 = Node10(10,test_microgrid)
+test_microgrid.add_node(n10)
 
-n11 = Node11(11,test_grid)
-test_grid.add_node(n11)
+n11 = Node11(11,test_microgrid)
+test_microgrid.add_node(n11)
 
-n12 = Node12(12,test_grid)
-test_grid.add_node(n12)
+n12 = Node12(12,test_microgrid)
+test_microgrid.add_node(n12)
 
-n13 = Node13(13,test_grid)
-test_grid.add_node(n13)
+n13 = Node13(13,test_microgrid)
+test_microgrid.add_node(n13)
 
-n14 = Node14(14,test_grid)
-test_grid.add_node(n14)
+n14 = Node14(14,test_microgrid)
+test_microgrid.add_node(n14)
 
-n15 = Node15(15,test_grid)
-test_grid.add_node(n15)
+n15 = Node15(15,test_microgrid)
+test_microgrid.add_node(n15)
 
-n16 = Node16(16,test_grid)
-test_grid.add_node(n16)
+n16 = Node16(16,test_microgrid)
+test_microgrid.add_node(n16)
 
-n17 = Node17(17,test_grid)
-test_grid.add_node(n17)
+n17 = Node17(17,test_microgrid)
+test_microgrid.add_node(n17)
 
-test_grid.set_next_node(n1)
-    
+test_microgrid.set_next_node(n1)
+
 def func(num):
     first = True
     while(num > 0):
-        if test_grid.next_node == n1:
-            test_grid.set_pwr_battery(random.randrange(101))
-            test_grid.set_pwr_load(random.randrange(101))
-            test_grid.set_pwr_pv()
-            test_grid.set_soc(random.randrange(101))
+        if test_microgrid.next_node == n1:
+            test_microgrid.dc_hub.battery.set_pwr_battery(random.randrange(0,101))
+            test_microgrid.ac_hub.load.set_pwr_load(random.randrange(0,101))
+            test_microgrid.dc_hub.pv.set_pwr_pv()
+            test_microgrid.dc_hub.battery.set_soc(random.randrange(0,101))
         print("-------------------------------------------------------------")
-        print("Current Node is : " + str(test_grid.next_node.id_number))
-        print("SoC is : " + str(test_grid.soc) + "%")
-        print("battery power is : " + str(test_grid.pwr_battery) )
-        print("PV power is : " + str(test_grid.pwr_pv))
-        print("Load / Grid is :" + str(test_grid.pwr_load))
-        test_grid.next_node.function()
+        print("Current Node is : " + str(test_microgrid.next_node.id_number))
+        print("SoC is : " + str(test_microgrid.dc_hub.battery.get_soc()) + "%")
+        print("battery power is : " + str(test_microgrid.dc_hub.battery.get_pwr_battery()) )
+        print("PV power is : " + str(test_microgrid.dc_hub.pv.get_pwr_pv()))
+        print("Load / Grid is :" + str(test_microgrid.ac_hub.load.get_pwr_load()))
+        test_microgrid.next_node.function()
         num -= 1
         print("-------------------------------------------------------------")
         
